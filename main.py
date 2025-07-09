a34257477de86b7fb9e56fdf74023472bbc46db8
AnmolSgill <anmolgill4u@gmail.com>
2025-07-08 20:57:47 -0400

    added getter method for player and piece to Player class

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ class Player:
         self.name = name # either CPU or player 1 or 2
         self.piece = piece # 1 or 2 / Red or Yellow
 
+
+    def get_name(self):
+        return self.name
+    
+    def get_piece(self):
+        return self.piece
+
+
 class Game:
     def __init__(self, grid):
         self.grid = grid
@@ -18,4 +26,11 @@ class Game:
 
 grid = Connect4Board(5, 5)
 game = Game(grid)
-game.play()
+
+
+player1 = game._players[0]
+player2 = game._players[1]
+print(player1.get_name())   # "Player #1"
+print(player1.get_piece())  # 1
+print(player2.get_name())   # "Player #2"
+print(player2.get_piece())  # 2
