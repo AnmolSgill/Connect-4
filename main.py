0106b13e353e7d65d870db1f133a2e274c2c7fb1
ss24610 <111543669+ss24610@users.noreply.github.com>
2025-07-22 15:25:48 -0400

    Update main.py
    supports visited and pruned branches

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from environment import *
 from minimax import AdversarialSearch
 from gemini_agent import GeminiAgent
+from random import randint
 
 class Player:
     def __init__(self, name, piece):
@@ -18,6 +19,7 @@ class Game:
     def __init__(self, grid):
         self.grid = grid
         self.connect = CONNECT_TARGET
+
         
         self.players = [
             Player('Player #1', PLAYER1_PIECE),
@@ -43,47 +45,191 @@ class Game:
 def setup_game(rows, columns):
     grid = Connect4Board(columns, rows)
     game = Game(grid)
+    
     return grid, game
+
+def run_game_vs_agent(player1_strategy, player2_strategy, rows, columns,algo):
+    grid, game = setup_game(rows, columns)
+    turn = 1
+    winner = False
+    visits = 0
+    pruning = []
+    adversary = AdversarialSearch(rows,columns)
+
+    if algo == "expectiminimax":
+        while not winner:     
+            for p in game.players:
+
+                if p.getPiece() == PLAYER1_PIECE:
+
+                    if turn % 4 == 3:
+                        moves = adversary.getValidMoves(grid.getGrid())
+                        i = randint(0,len(moves)-1)
+                        grid.placePlayerPiece(moves[i],p.getPiece())
+
+                    else:
+                        strategy = player1_strategy
+                        move = strategy(game, grid, p, turn)
+
+                else:
+
+                    if turn % 4 == 0:
+                        moves = adversary.getValidMoves(grid.getGrid())
+                        i = randint(0,len(moves)-1)
+                        grid.placePlayerPiece(moves[i],p.getPiece())
+                    
+                    else:
+
+
+                        strategy = player2_strategy
+                        move,visited = strategy(game, grid, p, turn)
+                        visits += visited 
+                        grid.placePlayerPiece(move, p.getPiece()) if move is not None else None
+
+
+                if grid.checkWin(CONNECT_TARGET, p.getPiece()):
+                    game.printConnect4Board()
+                    print(f"\n{p.getName()} wins!")
+                    winner = True
+                    print(f"Visited Nodes: {visits}")
+
+                    return visits
+                
+                if adversary.getValidMoves(grid.getGrid) == 0:
+                    print(f"Tie Game!")
+                    return
+
+                turn += 1
+    
+    if algo == "alphabeta":
+        pruned = []
+
+        while not winner:     
+            for p in game.players:
+
+                if p.getPiece() == PLAYER1_PIECE:
+
+                    
+                    strategy = player1_strategy
+                    move = strategy(game, grid, p, turn)
+
+                else:
+
+
+                    strategy = player2_strategy
+                    move,visited = strategy(game, grid, p, turn, pruned)
+                    visits += visited 
+                    grid.placePlayerPiece(move, p.getPiece()) if move is not None else None
+
+                if grid.checkWin(CONNECT_TARGET, p.getPiece()):
+                    game.printConnect4Board()
+                    print(f"\n{p.getName()} wins!")
+                    winner = True
+                    print(f"Visited Nodes: {visits} \nPruned Nodes: {len(pruned)}")
+                    return 
+                
+                if adversary.getValidMoves(grid.getGrid) == 0:
+                    print(f"Tie Game!")
+                    return
+
+
+                turn += 1
+    
+    if algo == "minimax":
+        while not winner:     
+            for p in game.players:
+
+                if p.getPiece() == PLAYER1_PIECE:
+
+                    
+                    strategy = player1_strategy
+                    move = strategy(game, grid, p, turn)
+
+                else:
+
+                    strategy = player2_strategy
+                    move,visited = strategy(game, grid, p, turn)
+                    visits += visited 
+                    grid.placePlayerPiece(move, p.getPiece()) if move is not None else None
+
+                if grid.checkWin(CONNECT_TARGET, p.getPiece()):
+                    game.printConnect4Board()
+                    print(f"\n{p.getName()} wins!")
+                    winner = True
+                    print(f"Visited Nodes: {visits}")
+                    return 
+                
+                if adversary.getValidMoves(grid.getGrid) == 0:
+                    print(f"Tie Game!")
+                    return
+
+                turn += 1
+        
+    
+
 
 def run_game(player1_strategy, player2_strategy, rows, columns):
     grid, game = setup_game(rows, columns)
-    turn = 0
+    turn = 1
     winner = False
+
 
     while not winner:
         for p in game.players:
-            strategy = player1_strategy if p.getPiece() == PLAYER1_PIECE else player2_strategy
-            move = strategy(game, grid, p, turn)
+
+            if p.getPiece() == PLAYER1_PIECE:
+
+                strategy = player1_strategy
+                move = strategy(game, grid, p, turn)
+
+            else:
+                strategy = player2_strategy
+                move = strategy(game, grid, p, turn)
+
             grid.placePlayerPiece(move, p.getPiece()) if move is not None else None
+            
+            
+
             if grid.checkWin(CONNECT_TARGET, p.getPiece()):
                 game.printConnect4Board()
                 print(f"\n{p.getName()} wins!")
                 winner = True
                 break
+
             turn += 1
 
 def human_strategy(game, grid, player, turn):
     game.simulatePlayerMove(player)
-    return None
+
+    
+    return 
 
 def gemini_strategy(difficulty):
     agent = GeminiAgent(player_piece=2, difficulty=difficulty)
     def strategy(game, grid, player, turn):
         return agent.choose_move(grid.getGrid())
+    
+    
     return strategy
 
 def ai_strategy(algorithm):
-    def strategy(game, grid, player, turn):
+    def strategy(game, grid, player, turn, pruned = None):
         ai = AdversarialSearch(grid.getRow(), grid.getColumns())
         if algorithm == "minimax":
-            _, move, _ = ai.minimax(grid.getGrid(), 6, True)
+            _, move, visited = ai.minimax(grid.getGrid(), 6, True,2,1)
         elif algorithm == "alphabeta":
-            _, move, _ = ai.alphaBetaPruning(grid.getGrid(), 6, -999999999, 999999999, True, [])
+            
+            _, move, visited = ai.alphaBetaPruning(grid.getGrid(), 6, -999999999, 999999999, True,2,1, pruned)
+            return move,visited
         elif algorithm == "expectiminimax":
-            _, move, _ = ai.expectiminiMax(grid.getGrid(), 6, True, turn)
+            _, move, visited = ai.expectiminiMax(grid.getGrid(), 6, True, turn,2,1)
         else:
             raise ValueError("Invalid AI algorithm")
-        return move
+        
+        
+        return move, visited
+    
+    
     return strategy
 
 def main_menu():
@@ -100,14 +246,14 @@ def main_menu():
 
     if choice in {"1", "2", "3"}:
         algo = {"1": "minimax", "2": "alphabeta", "3": "expectiminimax"}[choice]
-        run_game(human_strategy, ai_strategy(algo), 6, 7)
+        run_game_vs_agent(human_strategy, ai_strategy(algo), 6, 7,algo)
     elif choice == "4":
         difficulty = input("Gemini difficulty (easy / medium / hard): ").strip().lower()
         run_game(human_strategy, gemini_strategy(difficulty), 6, 7)
     elif choice in {"5", "6", "7"}:
         difficulty = input("Gemini difficulty (easy / medium / hard): ").strip().lower()
         algo = {"5": "minimax", "6": "alphabeta", "7": "expectiminimax"}[choice]
-        run_game(gemini_strategy(difficulty), ai_strategy(algo), 6, 7)
+        run_game_vs_agent(gemini_strategy(difficulty), ai_strategy(algo), 6, 7,algo)
     else:
         print("Invalid choice.")
 
